d11cd5d853771993cc9ce65c8ba00af89e43ad8d
matv99v <matv99v@gmail.com>
2018-09-13 11:25:25 +0300

    update webpack config

--- frontEnd/webpack.staging.js
+++ frontEnd/webpack.staging.js
@@ -1,17 +1,17 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 
-
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: 'production',
+    devtool: 'source-map',
     plugins: [
+        new UglifyJSPlugin({ sourceMap: true  }),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development'),
+            'process.env.NODE_ENV': JSON.stringify('production'),
             'process.env.HOST': JSON.stringify('109.95.32.134'),
-            'process.env.PORT': JSON.stringify('3001'),
+            'process.env.PORT': JSON.stringify('3001')
         })
     ]
-
 });
