a5c87dcdd23ffc1dcb17818d63b1ba7b0680f991
matv99v <matv99v@gmail.com>
2018-09-15 19:46:02 +0300

    fix some settings

--- frontEnd/webpack.common.js
+++ frontEnd/webpack.common.js
@@ -48,7 +48,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
+                use: ['babel-loader']
             },
             {
                 test: /\.scss$/,
