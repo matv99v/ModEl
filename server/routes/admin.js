2bc87a80fd4e80bc1d2615d1073a31c9deee03d8
matv99v <matv99v@gmail.com>
2018-09-26 19:56:07 +0300

    implement basic auth

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,6 +5,7 @@ const db = require('../database/db');
 const mysqlQueries = require('../database/mysqlQueries.js');
 const path = require('path');
 adminRouter.use(bodyParser.json());
+const auth = require('../auth/auth.js');
 
 
 
@@ -14,31 +15,9 @@ adminRouter.route('/')
     //     res.setHeader('Content-Type', 'application/json');
     //     next();
     // })
-    .get((req, res, next) => {
+    .get(auth, (req, res, next) => {
         res.sendFile(path.resolve(__dirname, '../public', 'admin-index.html'));
     });
-
-adminRouter.route('/goods')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        next();
-    })
-    .get((req, res, next) => {
-
-        // db.getQueryPromise(mysqlQueries.getAllGoods())
-        //     .then(rows => {
-        //         res.end(JSON.stringify(rows));
-        //     })
-        //     .catch(err => {
-        //         next(err);
-        //     });
-
-
-
-        res.send();
-    });
-
 
 
 module.exports = adminRouter;
