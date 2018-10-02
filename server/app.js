e945a30e97a3234836dc3d53df1c6192aeecad93
matv99v <matv99v@gmail.com>
2018-10-02 21:38:46 +0300

    implement sort barn

--- server/app.js
+++ server/app.js
@@ -1,25 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var api = require('./routes/api');
-var images = require('./routes/images');
-var admin = require('./routes/admin');
-
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const cors = require('cors'); // !!!!!!!
+const api = require('./routes/api');
+const images = require('./routes/images');
+const admin = require('./routes/admin');
 
-var app = express();
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -38,14 +37,14 @@ app.get('(/$|$)|(/catalog(/$|/*)?)|(/contacts($|/$)?)', (request, response) => {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
     console.log('!*--*-*!!!!*-*EXPRESS CATCHED AN ERROR');
     // set locals, only providing error in development
     res.locals.message = err.message;
