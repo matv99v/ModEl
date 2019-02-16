c5201311eedaa95265ce3e0e672da4c9af41a4ea
Vladimir Matveev <vmatveev@everydayhealthgroup.com>
2019-02-16 15:48:10 +0200

    Release 3 (#65)
    * refactor server to return amount of goods in category, refactor queries
    
    * r3 update project settings
    
    * WIP
    
    * be refactoring
    
    * r3 tidy up
    
    * r3 created script for generating script for creating symlinks depending on a platform
    
    * r3 updated symlinks creation
    
    * r3 - refactor validation
    
    * r3 tidy up
    
    * r3 refactor folders structure
    
    * r3 setting up initial values when good editing
    
    * r3 - good form first attempt
    
    * r3 - good form first attempt
    
    * r3 - remove auth for public request
    
    * r3 update db settings
    
    * r3 - update webpack to delete public folrder completely
    
    * r3 - update server to handle good editing
    
    * r3 - fix server db connection
    
    * r3 - bring back product name
    
    * finish with goods table
    
    * update middlewares

--- server/app.js
+++ server/app.js
@@ -22,10 +22,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(compression());
-app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
