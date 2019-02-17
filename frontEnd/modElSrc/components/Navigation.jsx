b5a6be0fc0ff1904e8281518390996e2d3e86b93
matv99v <matv99v@gmail.com>
2019-02-17 20:31:26 +0200

    adopt large numbers, use typehead in main app

--- frontEnd/modElSrc/components/Navigation.jsx
+++ frontEnd/modElSrc/components/Navigation.jsx
@@ -3,26 +3,29 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem, Badge, NavDropdown, MenuItem } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { fetchEnabledCategoriesAsync } from 'AliasModelSrc/redux/actions/catalog-actions';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import { withRouter } from 'react-router-dom';
 import './Navigation.scss';
 import BrandLogo from './common/BrandLogo';
 import helpers from '../../helpers/helpers';
-
-
-
+import api from 'AliasApi/api';
 
 
 class Navigation extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isOpen: false};
+        this.state = {
+            isOpen: false,
+            isLoading: false,
+            options: [],
+            typeheadFocusClass: '',
+        };
     }
-
 
     componentWillMount() {
         console.warn('fetching initial data, should be printed only one time');
         this.props.dispatch(fetchEnabledCategoriesAsync({enabled: true, excludeProductsCount: 0}));
     }
-
 
     handleNavToggle = () => {
         this.setState(prevState => ({isOpen: !prevState.isOpen}));
@@ -32,6 +35,38 @@ class Navigation extends React.Component {
         if (eventKey) {
             this.setState(() => ({isOpen: false}));
         }
+    }
+
+    handleGoodSearch = (query) => {
+        this.setState({isLoading: true});
+
+        const obj = {
+            table: 'products',
+            field: 'productName',
+            like: query,
+            enabled: true,
+        };
+
+        api.autocomplete(obj)
+            .then(json => this.setState({
+                isLoading: false,
+                options: json,
+            }));
+    }
+
+    handleGoodInputChange = ([selectedItem]) => {
+        if (selectedItem) {
+            this.props.history.push(`/catalog/${selectedItem.idCategory}/${selectedItem.idProduct}`);
+            this.typeahead.getInstance().clear();
+        }
+    }
+
+    handleTypeheadFocus = (e) => {
+        this.setState({typeheadFocusClass: 'typehead-expanded'});
+    }
+
+    handleTypeheadonBlur = (e) => {
+        this.setState({typeheadFocusClass: ''});
     }
 
     render() {
@@ -44,7 +79,14 @@ class Navigation extends React.Component {
         const categories = Object.keys(this.props.catalog).map(idCategory => this.props.catalog[idCategory]).sort(helpers.sortByPropName('CategoryName'));
 
         return (
-            <Navbar inverse onToggle={this.handleNavToggle} expanded={this.state.isOpen} onSelect={this.handleNavSelect} className="Navigation__Cnt">
+            <Navbar
+                fluid
+                inverse
+                onToggle={this.handleNavToggle}
+                expanded={this.state.isOpen}
+                onSelect={this.handleNavSelect}
+                className="Navigation__Cnt"
+            >
 
                 <Navbar.Header>
                     <Navbar.Brand>
@@ -57,9 +99,31 @@ class Navigation extends React.Component {
                 </Navbar.Header>
 
                 <Navbar.Collapse>
+                    <Navbar.Form
+                        className={`Navigation__search ${this.state.typeheadFocusClass}`}
+                        pullRight
+                    >
+                        <AsyncTypeahead
+                            ref={typeahead => this.typeahead = typeahead}
+                            labelKey='productName'
+                            isLoading={this.state.isLoading}
+                            options={this.state.options}
+                            onSearch={this.handleGoodSearch}
+                            onChange={this.handleGoodInputChange}
+                            placeholder='Search'
+                            bsSize='sm'
+                            onFocus={this.handleTypeheadFocus}
+                            onBlur={this.handleTypeheadonBlur}
+                            maxHeight='75vh'
+                            align={window.innerWidth <= 768 ? 'justify' : 'left'}
+                            clearButton
+                        />
+                    </Navbar.Form>
 
-                    <Nav pullRight>
-
+                    <Nav
+                        className={`Navigation__links ${this.state.typeheadFocusClass}`}
+                        pullRight
+                    >
                         <NavItem componentClass={Link} href="/" to='/'>
                             Инфо
                         </NavItem>
@@ -107,4 +171,5 @@ const mapStateToProps = (state) => ({
 });
 
 
-export default connect(mapStateToProps)(Navigation);
+const withRouterNavigation = withRouter(Navigation);
+export default connect(mapStateToProps)(withRouterNavigation);
